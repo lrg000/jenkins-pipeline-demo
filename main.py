96f4ee73269511e0b4a181dabe409336cef1b1dd
yu_jian_n <lrg000>
2026-07-01 11:49:54 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2026/7/1 11:40
+# @Author  : liuronggui
+# @File    : main.py
+print("🚀 Jenkins Pipeline 运行成功！")
+
+name = "Jenkins"
+print(f"Hello {name}")
